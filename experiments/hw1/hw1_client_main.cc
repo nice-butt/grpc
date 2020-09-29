230e3a9c5fbc755dd3de1fc7219a65e37b33069a
Weixiong Zheng <wzheng@carbon3d.com>
2020-09-28 23:18:43 -0700

    client builds

--- experiments/hw1/hw1_client_main.cc
+++ experiments/hw1/hw1_client_main.cc
@@ -6,13 +6,15 @@
 #include <chrono>
 #include <thread>
 
-#include "experiments/hw1.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
+
+#include "experiments/hw1/hw1.grpc.pb.h"
 
 namespace grpc::experiments {
 
 class GreeterClient {
  public:
-  GreeterClient(const std::shared_ptr<Channel>& channel)
+  GreeterClient(std::shared_ptr<Channel> channel)
       : stub_(Greeter::NewStub(channel)) {}
 
   std::string SayHi(const std::string& user, const std::string& message);
@@ -21,28 +23,29 @@ class GreeterClient {
   std::unique_ptr<Greeter::Stub> stub_;
 };
 
-Status GreeterClient::SayHi(const std::string& user, const std::string& message) {
+std::string GreeterClient::SayHi(const std::string& user, const std::string& message) {
   HiRequest request;
   request.set_name(user);
   std::stringstream ss;
-  ss << "Hi: " << message << ", at: " << std::ctime(std::time(nullptr));
+  std::time_t time_v = std::time(nullptr);
+  ss << "Hi: " << message << ", at: " << std::ctime(&time_v);
   request.set_hi_message(ss.str());
 
   // Receive hi_reply
   HiResponse response;
   ClientContext context;
-  return stub_->SayHi(&context, request, &response);
+  Status s = stub_->SayHi(&context, request, &response);
+  return s.ok() ? response.hi_reply() : "RPC failed";
 }
 
 }  // namespace grpc::experiments
 
 int main(int argc, char** argv) {
   const std::string target = "localhost:50051";
-  GreeterClient greeter(grpc::CreateChannel(target, grpc::InsecureChannelCredentials()));
-  using namespace std::chrono_literals;
+  grpc::experiments::GreeterClient greeter(grpc::CreateChannel(target, grpc::InsecureChannelCredentials()));
   while (true) {
-    greeter->SayHi("drz0", "hi until canceled");
-    std::this_thread::sleep_for(2s);
+    std::cout << greeter.SayHi("drz0", "hi until canceled") << "\n" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
   }
   return 0;
 }
